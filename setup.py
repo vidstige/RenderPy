b5fa803cadb11c695d3e09ad2deb95454414a200
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-11-10 21:06:26 +0100

    basic setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RenderPy',
+    version='1.0.0',
+    url='https://github.com/ecann/RenderPy',
+    author='Evan Cann',
+    packages=find_packages(),    
+    install_requires = [],
+)
